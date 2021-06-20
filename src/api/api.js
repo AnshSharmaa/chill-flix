c051c3efdcdc2f05d6e51b18e02a1837f41e7e5f
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-06-20 15:17:55 +0530

    feat: add api

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const API = axios.create({ baseURL: 'https://api.themoviedb.org/3' })
+const key = '?api_key=4d388821a0a9b0aded367d9dc2418d49'
+
+export const getPopular = () => API.get(`/movie/popular${key}&language=en-US&page=1`)
+export const getTopRated = () => API.get(`/movie/top_rated${key}&language=en-US&page=1`)
+export const getUpcoming = () => API.get(`/movie/upcoming${key}&language=en-US&page=1`)
+export const getNowPlaying = () => API.get(`/movie/now_playing${key}&language=en-US&page=1`)
+export const getTrendingWeekly = () => API.get(`trending/movie/week${key}`)
+export const getTrendingDay = () => API.get(`/trending/movie/day${key}`)
+
+/*
+https://api.themoviedb.org/3/movie/popular?api_key=4d388821a0a9b0aded367d9dc2418d49&language=en-US&page=1
+https://api.themoviedb.org/3/movie/top_rated?api_key=4d388821a0a9b0aded367d9dc2418d49&language=en-US&page=1
+https://api.themoviedb.org/3/movie/upcoming?api_key=4d388821a0a9b0aded367d9dc2418d49&language=en-US&page=1
+https://api.themoviedb.org/3/movie/now_playing?api_key=4d388821a0a9b0aded367d9dc2418d49&language=en-US&page=1
+https://api.themoviedb.org/3/trending/movie/week?api_key=4d388821a0a9b0aded367d9dc2418d49
+https://api.themoviedb.org/3/trending/movie/day?api_key=4d388821a0a9b0aded367d9dc2418d49
+*/
--- src/components/Lolomo/Movies/Movies.js
+++ src/components/Lolomo/Movies/Movies.js
@@ -1,66 +1,56 @@
+import { useState, useEffect } from 'react'
+
 import Movie from './Movie/Movie'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper.min.css'
 import 'swiper/components/navigation/navigation.min.css'
 import SwiperCore, { Pagination, Navigation } from 'swiper/core'
-
+import * as api from '../../../api/api'
 import './Movies.scss'
 
 SwiperCore.use([Pagination, Navigation])
 
 // Receives all the movies
 const Movies = () => {
-  const movie = {
-    adult: false,
-    backdrop_path: '/irlfhYtHfhZuYpsq2LAoh308NFe.jpg',
-    genre_ids: [18, 80],
-    id: 278,
-    original_language: 'en',
-    original_title: 'The Shawshank Redemption',
-    overview: 'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-    popularity: 48.852,
-    poster_path: '/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-    release_date: '1994-09-23',
-    title: 'The Shawshank Redemption',
-    video: false,
-    vote_average: 8.7,
-    vote_count: 19088,
+  const [movies, setMovies] = useState({ movies: [] })
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  if (loading) return 'Loading...'
+  if (error) return 'Error!'
+
+  async function getData() {
+    await api
+      .getPopular()
+      .then((response) => {
+        setMovies(response.data)
+        console.log(movies)
+        //console.log(response.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error)
+        setError(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
-  return (
+  return !movies.length ? (
+    <h1>Loading...</h1>
+  ) : (
     <>
       <Swiper slidesPerView={6} spaceBetween={0} slidesPerGroup={5} loop={true} loopFillGroupWithBlank={true} navigation={true} className='swiper'>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Movie movie={movie} />
-        </SwiperSlide>
+        {movies.results.map((movie) => (
+          <SwiperSlide>
+            <Movie movie={movie} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   )
