17ee8100c3d3ee4c7f9deddf821f1480c752fcfb
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-06-20 12:16:48 +0530

    feat: onhover movie view

--- src/components/Lolomo/Movies/Movie/Movie.js
+++ src/components/Lolomo/Movies/Movie/Movie.js
@@ -7,24 +7,10 @@ import './Movie.scss'
 
 const Movie = ({ movie }) => {
   const poster = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`
-  // adult: false,
-  //   backdrop_path: '/irlfhYtHfhZuYpsq2LAoh308NFe.jpg',
-  //   genre_ids: [18, 80],
-  //   id: 278,
-  //   original_language: 'en',
-  //   original_title: 'The Shawshank Redemption',
-  //   overview: 'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
-  //   popularity: 48.852,
-  //   poster_path: '/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-  //   release_date: '1994-09-23',
-  //   title: 'The Shawshank Redemption',
-  //   video: false,
-  //   vote_average: 8.7,
-  //   vote_count: 19088,
-  // }
+
   return (
     <div className='movie'>
-      <img src={poster} alt='poster' />
+      <img src={poster} alt='poster' className=' poster' />
 
       <div className='movie-details '>
         <h3>{movie.original_title}</h3>
